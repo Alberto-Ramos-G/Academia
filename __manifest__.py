f131d43d9eac6021cbc5cf38a4f303bf7f866795
Albert-21 <alberto.ah14@hotmail.com>
2022-08-17 10:56:12 -0500

    Realizacion de video 1

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 - QUADIT, SA DE CV(https://www.quadit.mx)
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+   "name":"Modulo Academia QUADIT",
+   "version":"15.0.0",
+   "depends":["base"],
+   "author":("QUADIT"),
+   "licence":"AGPL-3",
+   "summary":"""Modulo de practica QUADIT""",
+   "website":"www.quedit.mx",
+  "data":[
+        "security/ir.model.access.csv",
+        "views/view.xml",
+         ],
+   "installable":True,
+   "auto_install":False,
+}
---
+++ models/models.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 - QUADIT, SA DE CV(https://www.quadit.mx)
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import api,fields,models
+
+class academia_student(models.Model):
+    _name="academia.student"
+    _description="Gestion de estudiante"
+    name = fields.Char("Nombre",size=128)
+    last_name = fields.Char("Last_name",size=128)
