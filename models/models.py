a56b81414b52c983d110f850549cf4eeda8ad997
Albert-21 <alberto.ah14@hotmail.com>
2022-08-18 10:48:32 -0500

    Video8-Validacion-Curp1

--- models/models.py
+++ models/models.py
@@ -2,7 +2,7 @@
 # Copyright 2022 - QUADIT, SA DE CV(https://www.quadit.mx)
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api,fields,models
+from odoo import api,fields,models,exceptions
 
 class ResPartner(models.Model):
     _name ="res.partner"
@@ -25,5 +25,14 @@ class academia_student(models.Model):
         ],"Estado",default="draft")
     active = fields.Boolean("Activo",default=True)
     age = fields.Integer("Edad")
+    curp = fields.Char("CURP",size=18,required=True)
+    
     #Creacion de las relaciones 
     partner_id = fields.Many2one('res.partner','Escuela')
+    
+    @api.constrains('curp')
+    def check_curp(self):
+        for record in self:
+            if record.curp:
+                if len(record.curp) != 18:
+                    raise exception.ValidationError('La curp debe ser de 18 digitos')
