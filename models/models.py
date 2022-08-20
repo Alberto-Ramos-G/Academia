ed7eb8bc8bb0377b2f428e4b19f01c00b27c170d
Albert-21 <alberto.ah14@hotmail.com>
2022-08-20 12:20:48 -0500

    Video19

--- models/models.py
+++ models/models.py
@@ -3,25 +3,30 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models, exceptions
+import logging 
 
-class make_student_invoice (models.TransientModel):
+_logger = logging.getLogger(__name__)
+
+class make_student_invoice(models.TransientModel):
     _name = 'make.student.invoice'
     _description = 'Asistente para la generacion de facturas'
-    journal_id = fields. Many2one('account.journal', 'Diario', domain="[('type', '=', 'sale')]")
-    def make_invoices (self):
+    
+    journal_id = fields.Many2one('account.journal', 'Diario', domain="[('type', '=', 'sale')]")
+    
+    def make_invoices(self):
         active_ids = [self._context['active_ids']] 
         category_obj = self.env['product.category'] 
-        category_id = category_obj.search([('name', 'Factura colegiatura')])
+        category_id = category_obj.search([('name','=','Factura colegiatura')])
         for student_id in active_ids:
-            student_br = self.env('academia.student').search([('id', '=', student_id)])
+            student_br = self.env['academia.student'].search([('id', '=', student_id)])
             if student_br.state in ('draft', 'cancel'): 
                 raise exceptions.ValidationError('No puedes generar una factura para un estudiante expulsado o en borrador')
-                return False
+            _logger.info(category_id)
             if category_id:
                 product_obj = self.env['product.product'] 
                 products = product_obj.search([('categ_id', '=', category_id.id)])
-                invoice_obj = self.env['account.move'] 
                 partner_br = self.env['res.partner'].search([('student_id', '=', student_br.id)]) 
+                _logger.info(partner_br)
                 if partner_br:
                     partner_id =  partner_br[0].id
                     invoice_lines = []
@@ -30,7 +35,7 @@ class make_student_invoice (models.TransientModel):
                             'product_id': product.id,
                             'price_unit': product.list_price,
                             'quantity': 1,
-                            'account_id': product.catag_id.property_account_income_categ_id.id,
+                            'account_id': product.categ_id.property_account_income_categ_id.id,
                             'name': product.name + " ["+ str(product.default_code) + "]",
                         })
                         invoice_lines.append(xline)
@@ -47,6 +52,9 @@ class make_student_invoice (models.TransientModel):
                                 'invoice_ids': [(6,0,invoice_list)]                          
                         })
                         return True
+            else:
+                raise exceptions.ValidationError('No se pudo generar su factura')
+
 class ResPartner(models.Model):
     _name = "res.partner"
     _inherit = "res.partner"
@@ -98,6 +106,32 @@ class academia_student(models.Model):
     _name = "academia.student"
     _description = "Gestion de estudiante"
     _inherit = ['portal.mixin', 'mail.thread', 'mail.activity.mixin']
+    
+     # Funciones
+    def get_school_default(self):
+        school_id = self.env['res.partner'].search(
+            [('name', '=', 'Escuela por defecto')])
+        return school_id
+
+    @api.depends('calificaciones_id')
+    def calcular_promedio(self):
+        acum = 0.0
+        if len(self.calificaciones_id) > 0:
+            for xcal in self.calificaciones_id:
+                acum += xcal.calificacion
+                if acum:
+                    self.promedio = acum/len(self.calificaciones_id)
+        else:
+            self.promedio = 0.0
+            
+    @api.depends('invoice_ids') 
+    def calcular_amount(self):
+        acum = 0.0
+        for xcal in self.invoice_ids:
+            acum += xcal.amount_total
+            if acum:
+                self.amount_invoice = acum
+    
     name = fields.Char("Nombre", size=128, required=True,
                        track_visibility='onchange')
     last_name = fields.Char("Last_name", size=128)
@@ -115,23 +149,7 @@ class academia_student(models.Model):
     curp = fields.Char("CURP", size=18, copy=False)
     country = fields.Many2one('res.country', 'Pais',
                               related="partner_id.country_id")
-
-    # Funciones
-    def get_school_default(self):
-        school_id = self.env['res.partner'].search(
-            [('name', '=', 'Escuela por defecto')])
-        return school_id
-
-    @api.depends('calificaciones_id')
-    def calcular_promedio(self):
-        acum = 0.0
-        if len(self.calificaciones_id) > 0:
-            for xcal in self.calificaciones_id:
-                acum += xcal.calificacion
-                if acum:
-                    self.promedio = acum/len(self.calificaciones_id)
-        else:
-            self.promedio = 0.0
+    amount_invoice = fields.Float('Monto Facturado', digits=(14,2), compute="calcular_amount",store=True)
 
     # Creacion de las relaciones
     partner_id = fields.Many2one(
@@ -151,9 +169,8 @@ class academia_student(models.Model):
         for record in self:
             if record.curp:
                 if len(record.curp) != 18:
-                    raise exception.ValidationError(
-                        'La curp debe ser de 18 digitos')
-
+                    raise exception.ValidationError('La curp debe ser de 18 digitos')
+    
     def unlink(self):
         partner_obj = self.env['res.partner']
         partners = partner_obj.search([('student_id', 'in', self.ids)])
@@ -164,6 +181,7 @@ class academia_student(models.Model):
         res = super(academia_student, self).unlink()
         return res
 
+    @api.model
     def create(self, values):
         if values['name']:
             nombre = values['name']
@@ -171,7 +189,7 @@ class academia_student(models.Model):
                 values.update({
                     'name': values['name']+"(copy)"
                 })
-        res = super(academia_student, self).create(values)
+        res = super().create(values)
         partner_obj = self.env['res.partner']
         vals_to_partner = {
             'name': res['name']+" "+res["last_name"],
@@ -187,7 +205,6 @@ class academia_student(models.Model):
         self.state = 'process' 
         return True
 
-
     def cancel(self): 
         self.state = 'cancel' 
         return True
@@ -202,7 +219,7 @@ class academia_student(models.Model):
         self.state = 'draft' 
         return True
     
-    def generarFactura (self):
+    def generarFactura(self):
         return {
             'name': 'Generacion facturas', 
             'res_model': 'make.student.invoice', 
